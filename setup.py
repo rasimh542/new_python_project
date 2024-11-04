652528a50efd6f3eedfa4524b3e342a7ee706a17
rasimh542 <98226218+rasimh542@users.noreply.github.com>
2024-11-04 01:16:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="new_python_project_1",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="rasimh542@gmail.com",
     description="A short description of the package",
